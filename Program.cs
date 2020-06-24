4a8bb2f488de867e2aaa9cb702b576568e8829c3
Reshmapraveen <reshmapraveen28@gmail.com>
2020-06-24 15:40:56 -0600

    YOUR MESSAGE

--- Program.cs
+++ Program.cs
@@ -7,11 +7,25 @@ namespace c_sharp_intro
         static void Main(string[] args)
         {
             //Two slashes mark the beginning of a single-line comment.
-            // We are assigning string value to a variable.
+          
             string myName = "RESHMA";
-            /* We begin multi - line comments with a forward - slash and asterisk..*/
+            /* We begin multi - line comments with a forward - slash and 
+            asterisk..
 
-            Console.WriteLine("Hello, " + myName); 
+            We end multi line comments  with a  
+            asterisk forward - slash and.*/
+
+            Console.WriteLine("Hello, " + myName);
+            
+            Console.WriteLine("Press enter your name");
+              // We are assigning string value to a variable.
+            string userName = Console.ReadLine();
+            //show the user we collected the name
+            Console.WriteLine(" You have entered: " +userName);
+            //instead of traditional concatenation using the addition operator..
+            //Wecan use string interpolation placeholders) to embed values in 
+            //a string!
+            Console.WriteLine("Hey {0}, You are looking great today!" , userName);
         }
     }
 }
