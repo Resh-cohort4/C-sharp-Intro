5a5e90555d621d8543d3b7ebe1dac3ed6fce6d41
Reshmapraveen <reshmapraveen28@gmail.com>
2020-06-24 15:17:39 -0600

    Add an assignment; output concatenated hello string

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,TECHCareers!!");
+            //Two slashes mark the beginning of a single-line comment.
+            // We are assigning string value to a variable.
+            string myName = "RESHMA";
+            /* We begin multi - line comments with a forward - slash and asterisk..*/
+
+            Console.WriteLine("Hello, " + myName); 
         }
     }
 }
